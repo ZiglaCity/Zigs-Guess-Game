3ad463c68bf719056143e74b3227f02af44a45e8
ZiglaCity <ziglacity@gmail.com>
2024-11-19 13:16:46 +0000

    Defined Global Variables

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import tkinter as tk
 import random
 
+#Global variables initialization
+player1_name = "Player1"
+player2_name = "Player2"
+computer_name = "Computer"
+player1_score = 0
+player2_score = 0
+current_player = player1_name
+max_score = 11
+trials = 0
+game_mod_label = 0
+number_of_rounds = 2
+players_round = player1_name
+message = ""
+rounds_left = number_of_rounds
+random_number = None
+game_mode = 2
