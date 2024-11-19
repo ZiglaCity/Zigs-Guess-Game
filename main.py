00caf52b72ff9f4fd7b19329a4621414e906844e
ZiglaCity <ziglacity@gmail.com>
2024-11-19 13:18:22 +0000

    Defined fixed style for application

--- main.py
+++ main.py
@@ -22,3 +22,47 @@ game_mode = 2
 window_width = 600
 window_height = 650
 
+#define a fixed style to be used for all the widgets
+label_style = {
+    "font": ("Arial", 14, "italic"),
+    "bg": "#e6e6e6",
+    "fg": "#2b2b2b",
+    "padx": 8,
+    "pady": 8
+}
+
+button_style = {
+    "font": ("Comic Sans MS", 12, "bold"),
+    "bg": "#BBBBBB",
+    "fg": "#663399",
+    "bd": 3,
+    "relief": "ridge"
+}
+
+entry_style = {
+    "font": ("Comic Sans MS", 12),
+    "bg": "#ffebcd",
+    "fg": "#8b0000",
+    "bd": 2,
+    "relief": "groove"
+}
+
+frame_style = {
+    "bg": "#F5F5F5",
+    "bd": 2,
+    "relief": "sunken"
+}
+
+spinbox_style = {
+    "font": ("Arial", 12, "bold"),
+    "bg": "#f5f5f5",
+    "fg": "#333333",
+    "bd": 2,
+    "relief": "flat",
+    "highlightbackground": "#dcdcdc",
+    "highlightcolor": "#4CAF50",
+    "highlightthickness": 1,
+    "buttonbackground": "#4CAF50",
+    "buttondownrelief": "groove",
+    "buttonuprelief": "groove"
+}
